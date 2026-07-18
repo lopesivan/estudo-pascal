4e174bc1c079d3f303f3f3349da57ddf91c0b55c
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-07-18 16:16:01 -0300

    :fix: Primeira revisão do arquivo lazarus-3.0/modern-pascal-introduction/code-samples/compile.sh

--- lazarus-3.0/modern-pascal-introduction/code-samples/compile.sh
+++ lazarus-3.0/modern-pascal-introduction/code-samples/compile.sh
@@ -5,13 +5,27 @@ set -o pipefail
 
 echo fpc anotherunit.pas
 fpc anotherunit.pas
+ls -l anotherunit.o anotherunit.ppu
 echo
 
 echo fpc initialization_finalization.pas
 fpc initialization_finalization.pas
+ls -l initialization_finalization.o initialization_finalization.ppu
 echo
 
 echo fpc myunit.pas
 fpc myunit.pas
+ls -l myunit.o myunit.ppu
+echo
+
+echo fpc anon_functions_assignment_test.dpr
+fpc anon_functions_assignment_test.dpr
+ls -l anon_functions_assignment_test anon_functions_assignment_test.o
+echo
+
+echo fpc for_in_list.dpr
+fpc for_in_list.dpr
+ls -l for_in_list{,.o}
+echo
 
 exit 0
